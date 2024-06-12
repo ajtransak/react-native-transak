6ab915889118e39b87d21d8621036ce14fda4d37
ashutosh887 <ashutoshj887@gmail.com>
2024-06-13 01:50:18 +0530

    bug: transak events

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -5,8 +5,8 @@ import {
   Order,
   TransakWebView,
 } from '@transak/react-native-sdk';
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {ColorValue, StyleSheet, View} from 'react-native';
 import transakConfig from '../configs/transakConfig';
 import {AppStackParamsList} from '../types/screens';
 
@@ -14,26 +14,33 @@ interface PaymentScreenProps
   extends NativeStackScreenProps<AppStackParamsList, 'PAYMENT'> {}
 
 const PaymentScreen: React.FC<PaymentScreenProps> = ({navigation}) => {
+  const [borderColor, setBorderColor] = useState<ColorValue>('black');
+
   const onTransakEventHandler = (event: EventTypes, data: Order) => {
     switch (event) {
       case Events.ORDER_CREATED:
         // API call to register ORDER_CREATED
+        setBorderColor('yellow');
         break;
 
       case Events.ORDER_PAYMENT_VERIFYING:
         // API call to register ORDER_PAYMENT_VERIFYING
+        setBorderColor('green');
         break;
 
       case Events.ORDER_PROCESSING:
         // API call to register ORDER_PROCESSING
+        setBorderColor('purple');
         navigation.navigate('HOME');
         break;
 
       case Events.ORDER_COMPLETED:
+        setBorderColor('orange');
         navigation.navigate('HOME');
         break;
 
       case Events.ORDER_FAILED:
+        setBorderColor('red');
         navigation.navigate('ERROR');
         break;
 
@@ -42,27 +49,32 @@ const PaymentScreen: React.FC<PaymentScreenProps> = ({navigation}) => {
     }
   };
 
+  const styles = StyleSheet.create({
+    container: {
+      flexGrow: 1,
+      flex: 1,
+      backgroundColor: 'red',
+    },
+    transakWebview: {
+      flex: 1,
+      flexGrow: 1,
+    },
+    webviewContainer: {
+      borderColor,
+      borderWidth: 5,
+    },
+  });
+
   return (
     <View style={styles.container}>
       <TransakWebView
         transakConfig={transakConfig}
         onTransakEvent={onTransakEventHandler}
         style={styles.transakWebview}
+        containerStyle={styles.webviewContainer}
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    flex: 1,
-    backgroundColor: 'red',
-  },
-  transakWebview: {
-    flex: 1,
-    flexGrow: 1,
-  },
-});
 
 export default PaymentScreen;
